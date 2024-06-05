4ee42b6b4b1fd487dfab50092fd7ba4b5ad79a8a
Žan Sovič <soviczan7@gmail.com>
2024-06-05 16:43:18 +0200

    license

--- ctest.h
+++ ctest.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2024 Alexey Kutepov <reximkut@gmail.com>
+ *  Copyright 2024 Žan Sovič <soviczan7@gmail.com>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining
  *  a copy of this software and associated documentation files (the
